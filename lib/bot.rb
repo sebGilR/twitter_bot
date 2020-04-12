58b47b9cedea3292e79b4fdbaada66e8d3273096
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-04-11 23:24:35 -0500

    Set up get_tweets specs and method

--- lib/bot.rb
+++ lib/bot.rb
@@ -2,9 +2,18 @@ require 'twitter'
 require 'config'
 
 class TwitterBot
-  include 'config'
+  include Config
   def initialize
-    @client = client
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = CONSUMER_KEY
+      config.consumer_secret     = CONSUMER_SECRET
+      config.access_token        = ACCESS_TOKEN
+      config.access_token_secret = ACCESS_TOKEN_SECRET
+    end
     @tweets = []
+  end
+
+  def get_tweets(topic)
+    @tweets = @client.search("#{topic}", result_type: 'popular').take(5)
   end
 end
--- spec/bot_spec.rb
+++ spec/bot_spec.rb
@@ -2,5 +2,14 @@ require 'twitter'
 require_relative '../lib/bot.rb'
 
 RSpec.describe TwitterBot do
+  let(:bot) { described_class.new }
+  let(:tweets) { bot.get_tweets('javascript') }
 
+  describe '#get_tweets' do
+    it 'gets and stores tweets' do
+      bot_dbl = double('bot')
+      expect(bot_dbl).to receive(:get_tweets).with('topic').and_return([])
+      bot_dbl.get_tweets('topic')
+    end
+  end
 end

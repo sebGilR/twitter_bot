f0f2c6b60c97657a0a3e66da9dfa89313065a196
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-04-12 12:34:14 -0500

    create like and retweet specs and methods

--- lib/bot.rb
+++ lib/bot.rb
@@ -2,8 +2,9 @@ require 'twitter'
 require_relative 'config'
 
 class TwitterBot
-  attr_reader :tweets
   include Config
+  attr_reader :tweets
+
   def initialize
     @client = Twitter::REST::Client.new do |config|
       config.consumer_key        = CONSUMER_KEY
@@ -17,4 +18,16 @@ class TwitterBot
   def get_tweets(keyword)
     @tweets = @client.search("#{keyword}", result_type: 'popular').take(5)
   end
+
+  def like
+    @tweets.each { |tweet| @client.favorite(@tweets) }
+  end
+
+  def retweet
+    @tweets.each { |tweet| @client.retweet(tweet) }
+  end
 end
+
+# bot = TwitterBot.new
+# bot.get_tweets('bebes')
+# bot.tweets.each { |tweet| puts tweet.text + "\n\n --------------------" }
--- spec/bot_spec.rb
+++ spec/bot_spec.rb
@@ -2,14 +2,33 @@ require 'twitter'
 require_relative '../lib/bot.rb'
 
 RSpec.describe TwitterBot do
-  let(:bot) { described_class.new }
-  let(:tweets) { bot.get_tweets('javascript') }
+  let(:bot_dbl) { double('bot') }
 
   describe '#get_tweets' do
-    it 'gets and stores tweets' do
-      bot_dbl = double('bot')
+    it 'gets and saves tweets' do
       expect(bot_dbl).to receive(:get_tweets).with('topic').and_return([])
       bot_dbl.get_tweets('topic')
     end
   end
+
+  describe '#like' do
+    it 'likes saved tweets' do
+      expect(bot_dbl).to receive(:like).and_return('success')
+      bot_dbl.like
+    end
+  end
+
+  describe '#retweet' do
+    it 'retweets saved tweets' do
+      expect(bot_dbl).to receive(:retweet).and_return('success')
+      bot_dbl.retweet
+    end
+  end
+
+  describe '#follow' do
+    it 'follows the author of a tweet' do
+
+    end
+  end
+  
 end
